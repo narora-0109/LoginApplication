c399dc28a56e5ed0a7ad5bdefeacc123616d7194
Kai Berkemeyer <info@kaiberkemeyer.de>
2014-06-25 18:30:00 +0200

    User-validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  validates :username, presence: true, uniqueness: true
+  validates :surname, presence: true
+  validates :firstname, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
+  
   def full_name
   	"#{firstname} #{surname}"
   end
