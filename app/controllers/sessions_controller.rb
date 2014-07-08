0cf3081e23159f5a1fc694e9f93648b466b91bca
Kai Berkemeyer <kb@deltacity.net>
2014-07-08 11:00:49 +0200

    Various small modifications

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,12 +13,12 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to home_url
     else
-      redirect_to login_url, notice: "Invalid user/password combination"
+      redirect_to login_url, alert: "Invalid user/password combination"
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to login_url, alert: "Logged out"
+    redirect_to login_url, notice: "Logged out"
   end
 end
