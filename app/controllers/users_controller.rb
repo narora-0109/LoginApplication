10ad93086c71274fd9bbae9431bf7b6d6a39ffec
Kai Berkemeyer <info@kaiberkemeyer.de>
2014-08-20 19:29:07 +0200

    Alert bugfix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -64,6 +64,6 @@ class UsersController < ApplicationController
     end
     
     def redirect_if_not_allowed
-      redirect_to home_url, notice: "This action ist not allowed" if @current_user != @user && !@current_user.admin
+      redirect_to home_url, alert: "This action ist not allowed" if @current_user != @user && !@current_user.admin
     end
 end
